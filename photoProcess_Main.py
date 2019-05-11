1ed95dd40d3b9e46fe61d07d1c8742d5b82a7f20
madcreeper <sunjichen21@126.com>
2019-05-11 22:52:49 +0800

    19.5.11 update multiple angle output,better format

--- photoProcess_Main.py
+++ photoProcess_Main.py
@@ -4,16 +4,21 @@ import os
 import cv2
 from getJson import loadJson
 from calculateAngle import calcAngle
-angleList = ['torso_Rthigh',1,8,9]
+angleList = [['body_rLeg', 1, 8, 9, 85, 110],
+             ['body_lLeg', 1, 11, 12, 85, 110]]
+
 PGDOWN = 2228224
 SPACEBAR = 32
 ESC = 27
+
+
 def cv2Hitkey(k, waitTime):  # keyboard control
     t = cv2.waitKeyEx(waitTime)
-    #print(t)
+    # print(t)
     if (t == k):
         return True
     return False
+
 
 def init():
     print("Input Mode.\n Mode 1: Press 'SPACEBAR' to take screenshot \n Mode 2: Take screenshots automatically\n ESC to end program.\nPress any other key to quit.\n")
@@ -21,38 +26,48 @@ def init():
     print('Mode = ' + str(mode) + '\n')
     if (mode != 1 and mode != 2):
         sys.exit()
-    return(mode)
+    return (mode)
+
+
+def checkForAngle(arr):
+    for a in angleList:
+        curAngle = calcAngle(arr,a[1], a[2], a[3])
+        print(a,curAngle)
+
+
 # call OpenPosedemo.exe
 comm = ".\\bin\\OpenPosedemo.exe --render_pose 1 --display 0 --net_resolution 320x176 --image_dir tempImg/  --write_images genImg --write_json genJson --logging_level 3"
-#---------------------------------------
+# ---------------------------------------
 mode = init()
 cap = cv2.VideoCapture(0)
 totFrame = 0
 
 while(1):
 
-    ret,frame = cap.read()                                          # get a frame
-    cv2.imshow("capture", frame)                                    # show a frame
-    imgName = "tempImg/" + "test" + str(totFrame) + ".jpg"          # the directory of the temp image
+    ret, frame = cap.read()                                          # get a frame
+    # show a frame
+    cv2.imshow("capture", frame)
+    # the directory of the temp image
+    imgName = "tempImg/" + "test" + str(totFrame) + ".jpg"
 
-    if mode == 2 and cv2Hitkey(ESC,1):                                # this is to reduce LAG in auto mode, while avoid lag in manual mode
+    # this is to reduce LAG in auto mode, while avoid lag in manual mode
+    if mode == 2 and cv2Hitkey(ESC, 1):
         break
 
-    if ( (mode == 2) | cv2Hitkey(PGDOWN,1)) : #press pagedown to capture a photo
+    if ((mode == 2) | cv2Hitkey(PGDOWN, 1)):  # press pagedown to capture a photo
 
-        cv2.imwrite(imgName, frame) 
-        os.system(comm)                                             # pass shot picture to OpenPose                                                              # ... process the image / json
+        cv2.imwrite(imgName, frame)
+        # pass shot picture to OpenPose                                                              # ... process the image / json
+        os.system(comm)
         arr = loadJson("genJson/test" + str(totFrame) + "_keypoints.json")
-        #print(arr)
-        angle1 = calcAngle(arr, 11, 12, 13)
-        print(angle1)
-       #  for p in badPostions:
-           # print(hint[p])
-                                                                  # ... process the image / json
-        os.remove(imgName)                                          # remove the image
-       
-    if mode == 1 and cv2Hitkey(ESC,1):
+        # print(arr)
+
+        checkForAngle(arr)
+        # ... process the image / json
+        # remove the image
+        os.remove(imgName)
+
+    if mode == 1 and cv2Hitkey(ESC, 1):
         break
-   
+
     totFrame += 1
- 
